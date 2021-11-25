eab6ffae0bf484274fa05a3ccf8c15ea5d7e61ee
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-11-25 10:18:37 +0900

    Create [ D ] 가장 긴 부분 수열 구하기.py

---
+++ Contest/2021 경북대학교 Goricon/[ D ] 가장 긴 부분 수열 구하기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+sequence = list(map(int, input().split()))
+bit_count = [0] * 32
+
+def calc_bit(a):
+    temp = a
+    n = 0
+    while temp > 0:
+        bit_count[n] += (temp % 2)
+        temp = temp // 2
+        n += 1
+
+for item in sequence:
+    calc_bit(item)
+
+print(max(bit_count))
