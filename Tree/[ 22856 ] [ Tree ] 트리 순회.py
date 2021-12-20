7ee93b62daa10c40d021af3291a3aa188f107c0c
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-12-20 12:37:52 +0900

    Create [ 22856 ] [ Tree ] 트리 순회.py

---
+++ Tree/[ 22856 ] [ Tree ] 트리 순회.py
@@ -0,0 +1,40 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
+input = lambda: sys.stdin.readline().rstrip()
+left = dict()
+right = dict()
+
+N = int(input())
+parent = [0] * (N+1)
+node_count = 0
+for _ in range(N):
+    a, b, c = map(int, input().split())
+    left[a] = b
+    right[a] = c
+
+    if b != -1:
+        parent[b] = a
+        node_count += 1
+    if c != -1:
+        parent[c] = a
+        node_count += 1
+
+last_node = 0
+def traverse(node):
+    global last_node
+    if node == -1:
+        return
+    traverse(left[node])
+    last_node = node
+    traverse(right[node])
+
+traverse(1)
+edge_count = node_count * 2
+movement = 0
+while last_node != 1:
+    movement += 1
+    last_node = parent[last_node]
+print(edge_count - movement)
+    
